3aafc890c70d3d71a583c1f9b5be9f96560fb848
Jayasree Kurmannagari <jayasree0708@gmail.com>
2023-03-13 22:00:31 +0530

    Story2

--- controller/rentCalculator.test.js
+++ controller/rentCalculator.test.js
@@ -5,13 +5,13 @@ async function test() {
     let totalCharges = await calculateCharges(1);
     console.assert(totalCharges === 0, "Test case 1 passed");
 
-    // Test case 2: Customer with 1 rental for 5 days
+    // Test case 2: Customer with 2 fiction books and 2 novels rental for 5 days
     totalCharges = await calculateCharges(2);
-    console.assert(totalCharges === 5, "Test case 2 passed");
+    console.assert(totalCharges === 45, "Test case 2 passed");
 
-    // Test case 3: Customer with 2 rentals for 5 days each
+    // Test case 3: Customer with 2 regular books rentals for 5 days each
     totalCharges = await calculateCharges(3);
-    console.assert(totalCharges === 10, "Test case 3 passed");
+    console.assert(totalCharges === 15, "Test case 3 passed");
 }
 
 test();
